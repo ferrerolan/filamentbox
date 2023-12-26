38bcd2242ae86dfb503393c720933dc9ce86136a
Alan Ferreira <ferrerolan@MacBook-Pro-de-Alan.local>
2023-12-26 16:37:54 -0300

    make controllers

--- app/Filament/Resources/PropertyResource.php
+++ app/Filament/Resources/PropertyResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\PropertyResource\Pages;
 use App\Filament\Resources\PropertyResource\RelationManagers;
 use App\Models\Property;
+use Faker\Extension\CountryExtension;
 use Filament\Forms;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
