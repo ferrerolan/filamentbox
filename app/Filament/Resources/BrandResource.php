dfd34e40db868c548ac5330c8c9ebe027f559c08
Alan Ferreira <ferrerolan@icloud.com>
2024-01-09 20:07:22 -0300

    create view brands

---
+++ app/Filament/Resources/BrandResource.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BrandResource\Pages;
+use App\Filament\Resources\BrandResource\RelationManagers;
+use App\Models\Brand;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BrandResource extends Resource
+{
+    protected static ?string $model = Brand::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-stop';
+
+    protected static ?int $navigationSort = 1;
+
+    protected static ?string $navigationGroup = 'Shop';
+
+    public static function form(Form $form): Form
+{
+    return $form
+        ->schema([
+            Forms\Components\Group::make()
+                ->schema([
+                    Forms\Components\Group::make()
+                        ->schema([
+                            Forms\Components\Section::make('Brand Details')
+                                ->schema([
+                               
+                                    Forms\Components\TextInput::make('name')
+                                        ->required()
+                                        ->unique(),
+
+                                    Forms\Components\TextInput::make('slug'),
+        
+
+                                    Forms\Components\TextInput::make('url')
+                                        ->required()
+                                        ->label('Website URL')
+                                        ->unique()
+                                        ->columnSpan('full'),
+
+                                    Forms\Components\MarkdownEditor::make('description')
+                                        ->columnSpan('full')
+                                        ->columns(2),
+                                ]),
+
+                            Forms\Components\Group::make()
+                                ->schema([
+                                    Forms\Components\Toggle::make('is_visible')
+                                        ->label('visibility')
+                                        ->helperText('Enable or disable brand visibility')
+                                        ->default(true)
+                                        ->columnSpan('full'),
+                                ]),
+                            Forms\Components\Group::make()
+                                ->schema([
+                                    Forms\Components\Section::make('Color')
+                                        ->schema([
+                                            Forms\Components\ColorPicker::make('primary_hex')
+                                                ->label('Primary Color')
+                                        ])
+                                ])
+                        ]),
+                ]),
+        ]);
+}
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                
+                Tables\Columns\TextColumn::make('url')
+                    ->label('Website URL')
+                    ->sortable()
+                    ->searchable(),
+
+                Tables\Columns\IconColumn::make('primary_hex')
+                    ->label('Primary Color'),
+
+                Tables\Columns\IconColumn::make('is_visible')
+                    ->label('Visibility')
+                    ->boolean()
+                    ->sortable(),
+                
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->date()
+                    ->sortable(),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBrands::route('/'),
+            'create' => Pages\CreateBrand::route('/create'),
+            'edit' => Pages\EditBrand::route('/{record}/edit'),
+        ];
+    }    
+}
--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -2,17 +2,19 @@
 
 namespace App\Filament\Resources;
 
-use App\Enums\ProductTypeEnum;
-use App\Filament\Resources\ProductResource\Pages;
-use App\Filament\Resources\ProductResource\RelationManagers;
-use App\Models\Product;
 use Filament\Forms;
-use Filament\Resources\Form;
-use Filament\Resources\Resource;
-use Filament\Resources\Table;
 use Filament\Tables;
+use App\Models\Product;
+use Ramsey\Collection\Set;
+use Illuminate\Support\Str;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use App\Enums\ProductTypeEnum;
+use Filament\Resources\Resource;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\ProductResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ProductResource\RelationManagers;
 
 class ProductResource extends Resource
 {
@@ -22,6 +24,8 @@ class ProductResource extends Resource
 
     protected static ?string $navigationGroup = 'Work';
 
+    protected static ?int $navigationSort = 0;
+
     public static function form(Form $form): Form
     { 
         return $form
@@ -30,17 +34,44 @@ class ProductResource extends Resource
                     ->schema([
                         Forms\Components\Section::make('')
                             ->schema([
-                                Forms\Components\TextInput::make('name'),
-                                Forms\Components\TextInput::make('slug'),
+                                Forms\Components\TextInput::make('name')
+                                    ->required()
+                                    ->unique()
+                                    ->afterStateUpdated(function(string $operation, $state, Set $set) {
+                                        if ($operation === 'create') {
+                                            return;
+                                        }
+
+                                        $set('slug', Str::slug($state));
+                                    }),
+
+                                Forms\Components\TextInput::make('slug')
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->required(),
+
                                 Forms\Components\MarkdownEditor::make('description')
                                     ->columnSpan('full')
                             ])->columns(2),
 
                             Forms\Components\Section::make('Pricing & Inventory')
                             ->schema([
-                                Forms\Components\TextInput::make('sku'),
-                                Forms\Components\TextInput::make('price'),
-                                Forms\Components\TextInput::make('quantity'),
+                                Forms\Components\TextInput::make('sku')
+                                    ->label( "SKU  (Stock Keeping Unit)")
+                                    ->required()
+                                    ->unique(),
+
+                                Forms\Components\TextInput::make('price')
+                                    ->numeric()
+                                    ->required()
+                                    ->rules('regex:/A\d{1,6} (\. \d{0,2})?$/'),
+
+                                Forms\Components\TextInput::make('quantity')
+                                    ->numeric()
+                                    ->minValue(0)
+                                    ->maxValue(100)
+                                    ->required(),
+                                    
                                 Forms\Components\Select::make('type')
                                     ->options([
                                         'downloadable' => ProductTypeEnum::DOWNLOADABLE->value,
@@ -51,17 +82,29 @@ class ProductResource extends Resource
 
                     Forms\Components\Group::make()
                     ->schema([
-                        Forms\Components\Section::make('status')
-                            ->schema([
-                                Forms\Components\Toggle::make('is_visible'),
-                                Forms\Components\Toggle::make('is_featured'),
-                                Forms\Components\DatePicker::make('published_at'),
+                        // Forms\Components\Section::make('Status')
+                        //     ->schema([
+                        //         Forms\Components\Toggle::make('is_visible')
+                        //             ->label('Visibility')
+                        //             ->helperText('Enable or disable product visibility')
+                        //             ->default(true),
+
+                                Forms\Components\Toggle::make('is_featured')
+                                    ->label('Featured')
+                                    ->helperText('Enable or disable products features status'),
+
+                                Forms\Components\DatePicker::make('published_at')
+                                    ->label('Availability')
+                                    ->default(now())
                             ]),
                         
 
                         Forms\Components\Section::make('Image')
                             ->schema([
                                 Forms\Components\FileUpload::make('image')
+                                    ->directory('form-attachments')
+                                    ->preserveFilenames()
+                                    ->image()
                             ])->collapsible(),
 
                             Forms\Components\Section::make('Associations')
@@ -69,7 +112,7 @@ class ProductResource extends Resource
                                 Forms\Components\Select::make('brand_id')
                                     ->relationship('brand', 'name')
                             ]),
-                    ]),
+                   
             ]);
     }
 
@@ -78,17 +121,47 @@ class ProductResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\ImageColumn::make('image'),
-                Tables\Columns\TextColumn::make('name'),
-                Tables\Columns\TextColumn::make('brand.name'),
-                Tables\Columns\IconColumn::make('is_visible')->boolean(),
-                Tables\Columns\TextColumn::make('price'),
-                Tables\Columns\TextColumn::make('quantity'),
-                Tables\Columns\TextColumn::make('published_at'),
+
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('brand.name')
+                    ->searchable()
+                    ->sortable()
+                    ->toggleable(),
+
+                Tables\Columns\IconColumn::make('is_visible')->boolean()
+                    ->sortable()
+                    ->toggleable()
+                    ->label('Visibility')
+                    ->boolean(),
+
+                Tables\Columns\TextColumn::make('price')
+                    ->sortable()
+                    ->toggleable(),
+
+                Tables\Columns\TextColumn::make('quantity')
+                    ->sortable()
+                    ->toggleable(),
+
+                Tables\Columns\TextColumn::make('published_at')
+                    ->date()
+                    ->sortable(),
+                    
                 Tables\Columns\TextColumn::make('type'),
             ])
             ->filters([
-                //
-            ])
+                Tables\Filters\TernaryFilter::make('is_visible')
+                ->label('visibility')
+                ->boolean()
+                ->trueLabel('Only Visble Products')
+                ->falseLabel('Only Hidden Products'),
+
+                Tables\Filters\SelectFilter::make('brand')
+                    ->relationship('brand', 'name'),
+
+            ])  
             ->actions([
                 Tables\Actions\EditAction::make(),
             ])
